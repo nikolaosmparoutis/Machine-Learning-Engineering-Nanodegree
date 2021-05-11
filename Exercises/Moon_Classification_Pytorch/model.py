04682e04240cbe310cf35a22dfbda5138c4af791
Nikolaos Mparoutis <baroutisnk@gmail.com>
2021-05-11 21:41:43 +0300

    Add files via upload
    model.py

--- Exercises/Moon_Classification_Pytorch/model.py
+++ Exercises/Moon_Classification_Pytorch/model.py
@@ -15,7 +15,7 @@ class SimpleNet(nn.Module):
         super(SimpleNet, self).__init__()
 
         #         From my past experience i use this formula to find the optimal number of nodes
-        #         Nh=Ns(α∗(Ni+No))
+        #         Nh=Ns(α∗(Ni+No)) 
 
         #         Ni = number of input neurons.
         #         No = number of output neurons.
@@ -24,18 +24,19 @@ class SimpleNet(nn.Module):
         
         #         Nh = ... = [22,75]
         
-        # define all layers, here. I choose 4 layers andone Dropout.
+        #  Define all layers, here. I choose 3+1 layers.
         
         print("Default numbers for, input_dim, hidden_dim, output_dim", input_dim, hidden_dim, output_dim)
-        hid_layer1 = 50
-        hid_layer2 = 45
-        hid_layer3 = 40
+        hid_layer1 = hidden_dim
+        hid_layer2 = hidden_dim
         
         self.fc1 = nn.Linear(input_dim, hid_layer1)
-        self.fc2 = nn.Linear(hidden_1, hid_layer2)
+        self.bn1 = nn.LayerNorm(hid_layer1)
+        self.fc2 = nn.Linear(hid_layer1, hid_layer2)
+        self.bn2 = nn.LayerNorm(hid_layer2)
         self.fc3 = nn.Linear(hid_layer2, output_dim)
         # dropout prevents overfitting of data
-        self.drop = nn.Dropout(0.2)
+        self.drop = nn.Dropout(0.3)
         
     
     ## TODO: Define the feedforward behavior of the network
@@ -44,16 +45,17 @@ class SimpleNet(nn.Module):
            :param x: A batch of input features
            :return: A single, sigmoid activated value
          '''
-         
-        x = nn.BatchNorm1d(self.fc1(x)) # usually added before ReLU(as mentioned in the Batch Normalization paper). But there is no real standard 
-        x = F.leaky_relu(x, negative_slope=0.01)      # prevent dying relu
-        x = self.drop(0.2)
+        x = self.fc1(x)
+        x = self.bn1(x) # usually added before ReLU(as mentioned in the Batch Normalization paper), but not is not standard. 
+        x = F.leaky_relu(x, negative_slope=0.01)  # prevent dying relu and zeroing the gradient
         
-        x = nn.BatchNorm1d(self.fc2(x))
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
         x = F.leaky_relu(x, negative_slope=0.01)
-        x = self.drop(0.2)
         
-        x = nn.BatchNorm1d(self.fc3(x))
+        x = self.drop(x)
+        x = self.fc3(x)
         x = F.leaky_relu(x, negative_slope=0.01)
         
         return x
